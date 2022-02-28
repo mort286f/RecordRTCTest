4a7d28d3c8707bed7f2c54098e0aad2e0adf6a68
Morten Vestergaard <93192563+mortenvestergaard@users.noreply.github.com>
2022-02-28 08:22:51 +0100

    made observables

--- src/app/recording/record-video-popup-window/record-video-popup-window.component.ts
+++ src/app/recording/record-video-popup-window/record-video-popup-window.component.ts
@@ -1,7 +1,10 @@
-import { Component, Inject, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import * as RecordRTC from 'recordRTC';
 import { observable, Observable } from 'rxjs';
+import { VideoServiceService } from 'src/app/services/video-service.service';
+import { Stream } from 'stream';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 // import {VideoRecordingService} from '../../services/video-service.service';
@@ -11,26 +14,52 @@ import { observable, Observable } from 'rxjs';
 @Component({
   selector: 'app-record-video-popup-window',
   templateUrl: './record-video-popup-window.component.html',
-  styleUrls: ['./record-video-popup-window.component.css']
+  styleUrls: ['./record-video-popup-window.component.css'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
 
 export class RecordVideoPopupWindowComponent implements OnInit {
 
-  @ViewChild('videoElement') videoElement: any;
+@ViewChild('videoElement') videoElement: any;
 
   video: any;
   displayControls = true;
   isVideoRecording = false;
-  videoBlobUrl: any;
-  videoName: any;
+  videoBlob: any;
+  videoTitle: any;
+  blobUrl: any;
   private record_RTC: any;
+  // videoElement: HTMLVideoElement = {} as HTMLVideoElement;
   videoConf = { video: { facingMode: "user", width: 720}, audio: true};
   private videoSubscription: Observable<any> = new Observable();
 
-  constructor(private dialog: MatDialog, private videoStream: MediaStream) { }
 
+  //WebcamModule properties
+  public showWebcam = true;
+  public deviceId = String;
+  public videoOptions: MediaTrackConstraints = {
+    width: {ideal: 1024},
+    height: {ideal: 576}
+  };
+
+  constructor(private dialog: MatDialog, private videoStream: MediaStream, private videoService: VideoServiceService, private ref: ChangeDetectorRef, private sanitizer: DomSanitizer) {
+
+    this.videoService.getRecordedBlob().subscribe((data) => {
+      this.videoBlob = data.blob;
+      this.videoTitle = data.title;
+      this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(data.url);
+      this.ref.detectChanges();
+    });
+
+    this.videoService.getStream().subscribe((stream) => {
+      this.videoStream = stream;
+      this.ref.detectChanges();
+   });
+
+
+
+  }
   ngOnInit(): void {
-    console.log('initiated video popup')
   }
 
   //Error handling method, called with .then after VideoSuccessCallback is called
@@ -40,59 +69,55 @@ export class RecordVideoPopupWindowComponent implements OnInit {
 
 
   startRecording() {
-    console.log('recording started')
-    this.isVideoRecording = true;
-    const media = {
-      video: true,
-      audio: true
-    };
+    if(!this.isVideoRecording){
+      console.log('recording started')
+      this.isVideoRecording = true;
+      this.videoService.startRecording().subscribe(stream => {
+        this.video = this.videoElement = document.querySelector('#videoElement') as HTMLMediaElement;
+        this.video.srcObject = stream;
+        console.log(stream);
+        console.log('video src: ' + this.video.srcObject);
+        this.video.play();
+      });
+      // this.videoSubscription.subscribe(this.VideoSuccessCallback);
 
-
-
-
-    navigator.mediaDevices
-    .getUserMedia(media)
-    .then(this.VideoSuccessCallback.bind(this), this.handleError.bind(this));
+      // navigator.mediaDevices
+      // .getUserMedia(media)
+      // .then(this.VideoSuccessCallback.bind(this), this.handleError.bind(this));
+    }
   }
 
-  VideoSuccessCallback(stream: MediaStream) {
-  console.log('entered video success callback method')
-  this.videoStream = stream;
-  const record = new RecordRTC(stream,
-    {
-      mimeType: 'video/webm',
-      bitsPerSecond: 128000,
-    });
-    console.log("right before startRecording")
-  this.record_RTC.startRecording();
-  console.log("right after startRecording")
-  const videoElement: HTMLVideoElement = this.video.nativeElement;
-  videoElement.srcObject = stream;
-  console.log('stream blob: ' + videoElement.srcObject)
+  // VideoSuccessCallback(stream: MediaStream) {
+  // console.log('entered video success callback method')
+  // this.videoStream = stream;
+  // const record = new RecordRTC(stream,
+  //   {
+  //     mimeType: 'video/webm',
+  //     bitsPerSecond: 128000,
+  //   });
+  //   console.log("right before startRecording")
+  //   this.record_RTC.startRecording();
+  //   const videoElement: HTMLVideoElement = this.video.nativeElement;
+  //   videoElement.srcObject = stream;
+  //   const videoObs = {
+  //     next:  videoElement.srcObject,
+  //     error: console.log('Error occured in videoObs'),
+  //     complete: console.log('complete')
+  //   }
+  //   return videoObs;
   //WRONG AND OUTDATED:
   //video.srcObject = window.URL.createObjectURL(stream);
   //video.srcObject = window.URL.createObjectURL(videoBlob);
-  }
-
-  processVideo(audioVideoWebMURL: any) {
-    console.log('processing video');
-    const videoElement: HTMLVideoElement = this.video.nativeElement;
-    const recordRTC = this.record_RTC;
-    videoElement.src = audioVideoWebMURL;
-    const recordedBlob = recordRTC.getBlob();
-    recordRTC.getDataURL(function (dataURL: any) { });
-  }
+  // }
 
   stopRecording() {
-    console.log('stopping recording')
-    this.isVideoRecording = false;
-    const recordRTC = this.record_RTC;
-    recordRTC.stopRecording(this.processVideo.bind(this));
-    const stream = this.videoStream;
-    stream.getAudioTracks().forEach(track => track.stop());
-    stream.getVideoTracks().forEach(track => track.stop());
+    if(this.isVideoRecording) {
+      this.videoService.stopRecording()
+      this.video.srcObject = this.blobUrl;
+      console.log('blob logged: ' + this.blobUrl);
+      this.isVideoRecording = false;
+    }
   }
-
 
   CloseDialog(){
     this.dialog.closeAll();
--- src/app/services/video-service.service.ts
+++ src/app/services/video-service.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { promise } from 'protractor';
-import { Observable, Subject } from 'rxjs';
+import { observable, Observable, Subject } from 'rxjs';
 import { RecordedVideoOutput } from '../interfaces/recorded-video-output';
 import * as RecordRTC from 'recordrtc';
+import { Url } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -11,43 +12,75 @@ import * as RecordRTC from 'recordrtc';
 @Injectable()
 export class VideoServiceService {
 
-  constructor() { }
-
-  private stream: any;
+  private videoStream = new MediaStream;
   private recorder: any;
-  private interval: any;
-  private _stream = new Subject<MediaStream>();
+  private _videoStream = new Subject<MediaStream>();
   private _recorded = new Subject<RecordedVideoOutput>();
   private _recordedUrl = new Subject<string>();
-  private _recordingFailed = new Subject<string>();
 
-  StartRecording(conf: any): Promise<any>{
+  constructor() { }
 
-    var browser = <any>navigator;
-    if (this.recorder){
-      //Means recorder is already started or is already recording something. return nothing then
-      //TODO: Implement way to return when recorder is already running
-    }
-    return new Promise((resolve, reject) => {
-      browser.mediaDevices.getUserMedia(conf)
-      .then((stream: any) => {
-        this.stream = stream;
-        this.record();
-        resolve(this.stream);
-      }).catch((error: string) => {
-        error = 'error occured in starting the recording: '
-        reject;
+  getRecordedBlob(): Observable<RecordedVideoOutput> {
+    return this._recorded.asObservable();
+  }
+
+  getRecordedUrl(): Observable<string> {
+    return this._recordedUrl.asObservable();
+  }
+
+  getStream(): Observable<MediaStream> {
+    return this._videoStream.asObservable();
+  }
+
+private recordVideo() {
+  this.recorder = new RecordRTC(this.videoStream, {
+    type: 'video',
+    mimeType: 'video/webm',
+    bitsPerSecond: 128000,
+  });
+  this.recorder.startRecording();
+}
+
+  startRecording() {
+    return new Observable(observer => {
+      navigator.mediaDevices
+      .getUserMedia({audio: true, video: true})
+      .then((_videoStream) => {
+        this.recordVideo();
+        observer.next(_videoStream);
+      })
+      .catch((error) => {
+        console.log(observer.error(error));
       })
     })
   }
 
-  record() {
-    this.recorder = new RecordRTC(this.stream, {
-      type: 'video',
-      mimeType: 'video/webm',
-      bitsPerSecond: 44000
-    });
-    this.recorder.startRecording();
-    this._stream.next(this.stream);
+  processVideo(audioVideoWebMURL: any) {
+    console.log(audioVideoWebMURL);
+    const recordedBlob = this.recorder.getBlob();
+    this.recorder.getDataURL(function (dataURL: any) {});
+    const recordedName = encodeURIComponent('video_' + new Date().getTime() + '.webm');
+    this._recorded.next({blob: recordedBlob, url: audioVideoWebMURL, title: recordedName});
+    this.stopMedia();
   }
+
+  stopRecording() {
+    if(this.recorder) {
+
+      this.recorder.stopRecording(this.processVideo.bind(this))
+    }
+  }
+
+  private stopMedia() {
+    if (this.recorder) {
+      this.recorder = null;
+      if (this.videoStream) {
+        this.videoStream.getAudioTracks().forEach(track => track.stop());
+        this.videoStream.getVideoTracks().forEach(track => track.stop());
+        // this.videoStream.stop();
+        // this.videoStream = null;
+      }
+    }
+  }
+
 }
